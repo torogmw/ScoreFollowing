35d17c8536ff7a3b2550766670b3b166eebbd5b7
Minwei Gu <gothictoro@gmail.com>
2014-02-06 01:48:07 -0500

    first commit

---
+++ Source/Main.cpp
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "JuceHeader.h"
+#include "StockHMMlib.h"
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+
+    // ..your code goes here!
+
+  
+
+    return 0;
+}
